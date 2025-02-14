ef27482cf39a0ca675b2dc1828494a7f3b6ec31c
Devang Thaker <devangt101003@gamil.com>
2025-02-14 13:16:09 +0530

    image picked

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,41 @@ import { Image } from "expo-image";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from "react";
+
 
 const PlaceHolderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageSync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if(!result.canceled){
+      // console.log(result)
+      setSelectedImage(result.assets[0].uri);
+    }else{
+      alert("You did not select the image");
+    }
+  }
+
   return (
     <SafeAreaView style={style.container}>
       <View style={style.imgContainer}>
-        <ImageViewer imgSource={PlaceHolderImage} />
+        <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage}/>
       </View>
       <View style={style.footerContainer}>
-        <Button lable="Choose the Photo" />
-        <Button lable="Use this Photo" />
+        <Button theme="primary" lable="Choose the Photo" onPress={pickImageSync} />
+        <Button lable="Use this Photo" onPress={function (): void {
+          throw new Error("Function not implemented.");
+        } } />
       </View>
     </SafeAreaView>
   );
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,32 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native'
 import React from 'react'
+import  FontAwesome  from '@expo/vector-icons/FontAwesome'
 
 type Props = {
-    lable: string
+    lable: string,
+    theme?: 'primary',
+    onPress: () => void
 }
 
-const Button = ({lable} : Props) => {
+const Button = ({lable, theme, onPress} : Props) => {
+    if(theme === 'primary'){
   return (
-    <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert("You have pressed the button")}>
-        <Text style={styles.buttonLabel}>{lable}</Text>
+    <View style={[styles.buttonContainer, {borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18}]}>
+      <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+        <FontAwesome name='picture-o' size={18} color="#25292e" style={styles.buttonIcon} /> 
+        <Text style={[styles.buttonLabel, { color: '#2529e' }]}>{lable}</Text>
       </Pressable>
     </View>
   )
+}
+
+return(
+    <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={() => alert("Button is pressed")}>
+            <Text style={styles.buttonLabel}>{lable}</Text>
+        </Pressable>
+    </View>
+)
 }
 
 export default Button
@@ -33,6 +47,9 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: 'row',
+    },
+    buttonIcon: {
+        paddingRight: 8
     },
     buttonLabel: {
       color: '#fff',
