3769ca5e1599a80d7fed89126bf5a4ed1b2940bb
Devang Thaker <devangt101003@gamil.com>
2025-02-14 12:38:09 +0530

    second commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ const TabLaytout = () => {
                 backgroundColor: '#25292e'
             }
         }}>
-            <Tabs.Screen name='index' options={{ title: 'Home', tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} /> }} />
+            <Tabs.Screen name='index' options={{ title: 'Sticker Smash', tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} /> }} />
             <Tabs.Screen name='about' options={{ title: 'About', tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
           ) }} />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceHolderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
   return (
-    <View style={style.container}>
-      <Text style={style.text}>Home Screen</Text>
-      <Link href="/about" style={style.button}>About Us</Link>
-    </View>
+    <SafeAreaView style={style.container}>
+      <View style={style.imgContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View style={style.footerContainer}>
+        <Button lable="Choose the Photo" />
+        <Button lable="Use this Photo" />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,14 +26,12 @@ const style = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center'
   },
-  text: {
-    color: '#fff'
+  imgContainer: {
+    flex: 1
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff'
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center'
   }
 })
